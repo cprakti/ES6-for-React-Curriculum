e506301243b9cfa002dffcd253541888ced4a00e
Chris Praktish <chris.praktish@gmail.com>
2016-10-31 15:19:12 -0400

    Refactor all files to comply with ES6

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -1,17 +1,16 @@
-var axios = require('axios');
+const axios = require('axios')
 
-var _baseURL = 'http://api.openweathermap.org/data/2.5/';
-var _APIKEY = 'b714ec74bbab5650795063cb0fdf5fbe';
+const _baseURL = 'http://api.openweathermap.org/data/2.5/'
+const _APIKEY = 'b714ec74bbab5650795063cb0fdf5fbe';
 
 function prepRouteParams (queryStringData) {
   return Object.keys(queryStringData)
-    .map(function (key) {
-      return key + '=' + encodeURIComponent(queryStringData[key]);
-    }).join('&')
+    .map((key) => key + '=' + encodeURIComponent(queryStringData[key]))
+    .join('&')
 }
 
 function prepUrl (type, queryStringData) {
-  return _baseURL + type + '?' + prepRouteParams(queryStringData);
+  return `${_baseURL + type}?${prepRouteParams(queryStringData)}`
 }
 
 function getQueryStringData (city) {
@@ -23,27 +22,18 @@ function getQueryStringData (city) {
   }
 }
 
-function getCurrentWeather (city) {
-  var queryStringData = getQueryStringData(city);
-  var url = prepUrl('weather', queryStringData)
+export async function getCurrentWeather (city) {
+  const queryStringData = getQueryStringData(city);
+  const url = prepUrl('weather', queryStringData)
 
   return axios.get(url)
-    .then(function (currentWeatherData) {
-      return currentWeatherData.data
-    })
+    .then((currentWeatherData) => currentWeatherData.data)
 }
 
-function getForcast (city) {
-  var queryStringData = getQueryStringData(city);
-  var url = prepUrl('forecast/daily', queryStringData)
-
+export async function getForcast (city) {
+  const queryStringData = getQueryStringData(city);
+  const url = prepUrl('forecast/daily', queryStringData)
+  
   return axios.get(url)
-    .then(function (forecastData) {
-      return forecastData.data
-    })
+    .then((forecastData) => forecastData.data)
 }
-
-module.exports = {
-  getCurrentWeather: getCurrentWeather,
-  getForcast: getForcast
-};
